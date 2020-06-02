3dfeb9c1c4c50e606d3901c0ae30c7cad332344a
JoseLRiano <joselriano@yahoo.com>
2020-06-02 03:53:22 -0400

    Updates

---
+++ precache-manifest.51a60d95aa976d141c2813971e812534.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "99d34fdaad56d43e6b4c143e928329d7",
+    "url": "/DadJokes/index.html"
+  },
+  {
+    "revision": "5f9da66d62c2856aab4a",
+    "url": "/DadJokes/static/css/main.96461d35.chunk.css"
+  },
+  {
+    "revision": "8744e4ee268a1afbbcf7",
+    "url": "/DadJokes/static/js/2.757d0cb2.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/DadJokes/static/js/2.757d0cb2.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "5f9da66d62c2856aab4a",
+    "url": "/DadJokes/static/js/main.5a17cfde.chunk.js"
+  },
+  {
+    "revision": "5f2cc6a24fd3e11b986b",
+    "url": "/DadJokes/static/js/runtime-main.db7a4da7.js"
+  }
+]);
